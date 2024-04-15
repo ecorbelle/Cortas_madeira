bebe333960dcb3e4832a1e51c6eeac91fef30cb4
edujose <eduardo.corbelle@usc.es>
2024-04-15 14:17:25 +0200

    Interpola datos de coniferas para 1994 e crea grafico de areas apiladas

--- Codigo/Cortas_SerieHistorica.R
+++ Codigo/Cortas_SerieHistorica.R
@@ -3,6 +3,7 @@
 
 library(data.table)
 library(ggplot2)
+library(ggokabeito)
 
 # Carga de datos de cortas ----
 
@@ -13,14 +14,24 @@ cortas1 <- rbind(
 )
 
 cortas2 <- dcast(cortas1, ano + provincia ~ grupo, value.var = "volume", fill = 0)
+# Interpola datos de coníferas e volume total para o ano 1994
+for ( i in levels (factor(cortas2$provincia))) {
+  cortas2$Coníferas[cortas2$provincia == i & cortas2$ano == 1994] = 
+    mean(c(cortas2$Coníferas[cortas2$provincia == i & 
+                               (cortas2$ano == 1993 | cortas2$ano == 1995)]))
+}
+
 cortas2[ , `:=`(Outras = Cupulíferas + Outras + Frondosas - Eucalipto,
-               Total = Cupulíferas + Outras + Frondosas + Coníferas)]
+                Total = Cupulíferas + Outras + Frondosas + Coníferas)]
+
+
 cortas3 <- melt(cortas2, 
                 id.vars = c("ano", "provincia"), 
                 measure.vars = c("Coníferas", "Eucalipto", "Outras", "Total"),
                 variable.name = "grupo",
                 value.name = "volume")
 cortas <- cortas3[ , .(volume = sum(volume)), .(ano, grupo)]
+
 cortas$grupo <- factor(cortas$grupo, levels = c("Total", "Eucalipto", "Coníferas", "Outras"))
 cortas3$grupo <- factor(cortas3$grupo, levels = c("Total", "Eucalipto", "Coníferas", "Outras"))
 
@@ -39,6 +50,30 @@ g.total <- ggplot(cortas, aes(x = ano, y = volume/1e6)) +
   theme_light() +
   theme(plot.caption = element_text(size = 7, colour = "grey40"),
         axis.title.x = element_blank())
+
+g.total.areas <- ggplot(cortas[grupo != "Total",], aes(x = ano, y = volume/1e6)) +
+  geom_area(aes(fill = grupo), color = "white", lwd = .1) +
+  # geom_text(data = labels, mapping = aes(x = x, y = y, label = label), col = "white") +
+  scale_fill_okabe_ito(order = c(2,3,1)) +
+  scale_y_continuous(breaks = seq(0, 10, 2)) +
+  labs(title   = "Galicia. Cortas de madeira por grupos de especies, 1960-2020",
+       subtitle= "(Millóns de m³)",
+       fill    = "Grupo de especies",
+       caption = "Eduardo Corbelle, 2024. CC-BY 4.0\nDatos: Estadística Forestal de España (MITECO)") +
+  theme(legend.position = "inside",
+        legend.position.inside = c(.15,.76),
+        panel.background = element_rect(fill = "white"),
+        legend.title = element_text(size = 10),
+        panel.grid = element_blank(),
+        panel.grid.major.y = element_line(color = "grey80", linewidth = 0.3),
+        panel.grid.minor.y = element_line(color = "grey80", linewidth = 0.1),
+        axis.ticks.length.y = unit(0, "mm"), 
+        axis.ticks.length.x = unit(2, "mm"),
+        axis.title = element_blank(),
+        axis.line.x.bottom = element_line(color = "black"),
+        axis.text.x = element_text(size = 10),
+        plot.caption = element_text(size = 7, colour = "grey40")) 
+
 
 
 g.prov <- ggplot(cortas3, aes(x = ano, y = volume/1e6)) +
@@ -59,6 +94,8 @@ g.prov <- ggplot(cortas3, aes(x = ano, y = volume/1e6)) +
 
 
 
+
+
 # Exportación ----
 png("Cortas1.png", width = 15, height = 10, units = "cm", res = 300)
  print(g.total)
@@ -66,4 +103,8 @@ dev.off()
 
 png("Cortas1b.png", width = 15, height = 10, units = "cm", res = 300)
 print(g.prov)
+dev.off()
+
+png("Cortas1c.png", width = 15, height = 10, units = "cm", res = 300)
+print(g.total.areas)
 dev.off()
